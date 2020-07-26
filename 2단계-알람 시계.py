779c75460a425eaed838ca272df3240781399c3c
zzangw00 <ckddn4679@naver.com>
2020-07-26 22:08:47 +0900

    알람 시계
    BOJ problem by Python

---
+++ 2단계-알람 시계.py
@@ -0,0 +1,13 @@
+H, M = map(int, input().split())
+
+if H > 0 and H < 24:
+    if M >=45 and M < 60:
+        print(H, (M - 45))
+    else:
+        print((H - 1), ((60 + M) - 45))
+
+elif H == 0:
+    if M >=45 and M < 60:
+        print(H, M - 45)
+    else:
+        print(23, ((60 + M) - 45))
