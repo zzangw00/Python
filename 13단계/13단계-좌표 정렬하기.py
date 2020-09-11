957e107411602c119297f0b5071ca70d543a2160
zzangw00 <ckddn4679@naver.com>
2020-09-11 12:25:32 +0900

    좌표 정렬하기
    BOJ problem by Python

---
+++ 13단계/13단계-좌표 정렬하기.py
@@ -0,0 +1,13 @@
+import sys
+def solution(n):
+    arr = []
+    for i in range(n):
+        arr.append(list(map(int, sys.stdin.readline().split())))
+
+    arr = sorted(arr, key = lambda x: (x[0], x[1]))
+
+    for i, j in arr:
+        print(i, j)
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    solution(n)
